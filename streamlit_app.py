071bd11ac77a7641c340b83d4187bf1d95c63398
Rohan Dilip Yadav <ron11296@gmail.com>
2024-01-15 13:05:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,38 +39,38 @@ user_input = {}
 st.title("Telecom Churn Prediction")
 
 # Using st.form to wrap the input collection
-    st.button("user_input_form")
-    # Collect user inputs on the first page
-    for col, input_info in input_columns.items():
-        if input_info['type'] == 'number_input':
-            user_input[col] = st.number_input(f"{input_info['text']}", value=0, step=1)
-        elif input_info['type'] == 'radio':
+ st.button("user_input_form")
+# Collect user inputs on the first page
+for col, input_info in input_columns.items():
+    if input_info['type'] == 'number_input':
+        user_input[col] = st.number_input(f"{input_info['text']}", value=0, step=1)
+    elif input_info['type'] == 'radio':
             # Map 'NO' to 0 and 'YES' to 1
-            user_input[col] = 1 if st.radio(f"{input_info['text']}", options=input_info['options'], key=col) == 'YES' else 0
+        user_input[col] = 1 if st.radio(f"{input_info['text']}", options=input_info['options'], key=col) == 'YES' else 0
 
-    # Predict button
-    if st.form_submit_button("Predict"):
-        # Create a DataFrame from user input
-        input_df = pd.DataFrame([user_input])
-        print(input_df, "input_df")
-        # Make prediction
-        prediction = loaded_model.predict(pd.DataFrame([user_input]))[0]
+# Predict button
+if st.form_submit_button("Predict"):
+    # Create a DataFrame from user input
+    input_df = pd.DataFrame([user_input])
+    print(input_df, "input_df")
+# Make prediction
+prediction = loaded_model.predict(pd.DataFrame([user_input]))[0]
 
-        # Set the session state variable to True to show the second page
-        st.session_state.show_result = True
+# Set the session state variable to True to show the second page
+st.session_state.show_result = True
 
 # Check if the button to show the second page is clicked
 if st.session_state.show_result:
-    # Page break
-    st.markdown("---")
+# Page break
+st.markdown("---")
 
-    # Second page layout
-    st.title("Result")
+# Second page layout
+st.title("Result")
 
-    # Display result with animation or other visualizations
-    if prediction == 1:
-        st.markdown("This customer is likely to <span style='color:red; font-size:32px;'>CHURN</span>", unsafe_allow_html=True)
-        st.image('https://i.gifer.com/3Qxw.gif', caption=" ", width=500)
-    else:
-        st.markdown("This customer is likely to <span style='color:red; font-size:32px;'>NOT CHURN</span>", unsafe_allow_html=True)
-        st.image("https://i.gifer.com/5dk.gif", caption=" ", width=500)
+# Display result with animation or other visualizations
+if prediction == 1:
+    st.markdown("This customer is likely to <span style='color:red; font-size:32px;'>CHURN</span>", unsafe_allow_html=True)
+    st.image('https://i.gifer.com/3Qxw.gif', caption=" ", width=500)
+else:
+    st.markdown("This customer is likely to <span style='color:red; font-size:32px;'>NOT CHURN</span>", unsafe_allow_html=True)
+    st.image("https://i.gifer.com/5dk.gif", caption=" ", width=500)
